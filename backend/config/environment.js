71d078cae62d85549ab2601d89729aa96bbc9f6c
Reisli Hysa <reislihysa@Isabellas-iMac.local>
2021-12-27 22:42:54 +0000

    environment

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -1,3 +1,7 @@
-export const port = 4000
-export const dbURI = 'mongodb://127.0.0.1:27017/journi-api'
-export const secret = 'tbd'
+import dotenv from "dotenv";
+dotenv.config();
+
+export const port = process.env.PORT || 4000;
+export const dbURI =
+  process.env.MONGODB_URI || "mongodb://localhost/journi-api";
+  export const secret = process.env.SECRET ||
